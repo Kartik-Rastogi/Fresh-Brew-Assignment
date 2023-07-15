b56b61aca1ff806ba6afd362e8f5c872e99a16c1
Kartik Rastogi <krastogi2611@gmail.com>
2023-07-15 21:15:12 +0530

    First Commit

---
+++ src/screens/Dashboard/Dashboard.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import "./style.css";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="div">
+        <div className="overlap">
+          <div className="group">
+            <div className="overlap-group">
+              <div className="text-wrapper">Home</div>
+              <img className="vector" alt="Vector" src="/assets/img1/vector-21.svg" />
+            </div>
+            <img className="line" alt="Line" src="/assets/img1/line-1.svg" />
+            <div className="text-wrapper-2">Neha Bhat</div>
+            <div className="text-wrapper-3">Bookings</div>
+            <div className="text-wrapper-4">Queries</div>
+            <div className="text-wrapper-5">Schedules</div>
+            <div className="text-wrapper-6">Services</div>
+            <div className="text-wrapper-7">Profile</div>
+            <div className="invite-earn">Invite &amp; Earn</div>
+            <div className="text-wrapper-8">Rewards</div>
+            <div className="text-wrapper-9">Logout</div>
+            <img className="img" alt="Line" src="/assets/img1/line-3.svg" />
+            <img className="line-2" alt="Line" src="/assets/img1/line-3.svg" />
+            <img className="vector-2" alt="Vector" src="/assets/img1/vector-20.svg" />
+            <img className="vector-3" alt="Vector" src="/assets/img1/vector-19.svg" />
+            <img className="vector-4" alt="Vector" src="/assets/img1/vector-18.svg" />
+            <img className="vector-5" alt="Vector" src="/assets/img1/vector-17.svg" />
+            <img className="vector-6" alt="Vector" src="/assets/img1/vector-16.svg" />
+            <img className="vector-7" alt="Vector" src="/assets/img1/vector-15.svg" />
+            <img className="vector-8" alt="Vector" src="/assets/img1/vector-14.svg" />
+            <img className="ellipse" alt="Ellipse" src="/assets/img1/ellipse-7.png" />
+          </div>
+          <img className="vector-9" alt="Vector" src="/assets/img1/vector-1.svg" />
+        </div>
+        <h1 className="h-1">Hi Neha!</h1>
+        <div className="group-2">
+          <div className="text-wrapper-10">Meet similar mentors</div>
+          <div className="overlap-2">
+            <img className="ellipse-2" alt="Ellipse" src="/assets/img1/ellipse-10.png" />
+            <img className="ellipse-3" alt="Ellipse" src="/assets/img1/ellipse-8.png" />
+            <img className="ellipse-4" alt="Ellipse" src="/assets/img1/ellipse-9.png" />
+          </div>
+          <img className="vector-10" alt="Vector" src="/assets/img1/vector-13.svg" />
+          <img className="vector-11" alt="Vector" src="/assets/img1/vector-12.svg" />
+        </div>
+        <div className="overlap-wrapper">
+          <div className="overlap-3">
+            <div className="rectangle" />
+            <div className="text-wrapper-11">Welcome to Mentor Bridge</div>
+            <img className="line-3" alt="Line" src="/assets/img1/line-8.svg" />
+            <img className="line-4" alt="Line" src="/assets/img1/line-8.svg" />
+            <img className="line-5" alt="Line" src="/assets/img1/line-8.svg" />
+            <img className="line-6" alt="Line" src="/assets/img1/line-8.svg" />
+            <img className="vector-12" alt="Vector" src="/assets/img1/vector-11.svg" />
+            <img className="vector-13" alt="Vector" src="/assets/img1/vector-8.svg" />
+            <img className="vector-14" alt="Vector" src="/assets/img1/vector-8.svg" />
+            <img className="vector-15" alt="Vector" src="/assets/img1/vector-8.svg" />
+            <p className="p">Your Journey as a mentor begins here</p>
+            <div className="text-wrapper-12">Add availability</div>
+            <div className="text-wrapper-13">Complete your profile</div>
+            <div className="text-wrapper-14">Share a post</div>
+            <div className="text-wrapper-15">Connect payout</div>
+          </div>
+        </div>
+        <footer className="footer">
+          <div className="overlap-4">
+            <div className="text-wrapper-16">Help center</div>
+            <div className="text-wrapper-17">About us</div>
+            <div className="text-wrapper-18">FAQs</div>
+            <img className="vector-16" alt="Vector" src="/assets/img1/vector-7.svg" />
+            <img className="vector-17" alt="Vector" src="/assets/img1/vector-6.svg" />
+            <img className="vector-18" alt="Vector" src="/assets/img1/vector-5.svg" />
+            <img className="vector-19" alt="Vector" src="/assets/img1/vector-4.svg" />
+            <p className="element-mentorbridge">
+              <span className="span">2023 M</span>
+              <span className="text-wrapper-19">entorbridge. </span>
+              <span className="span">All Rights Reserved.</span>
+            </p>
+            <div className="text-wrapper-20">Contact us</div>
+            <div className="text-wrapper-21">Privacy policy</div>
+            <div className="text-wrapper-22">Terms of use</div>
+            <img className="vector-20" alt="Vector" src="/assets/img1/vector-3.svg" />
+          </div>
+        </footer>
+        <div className="div-2">
+          <img className="vector-21" alt="Vector" src="/assets/img1/vector-2.svg" />
+          <div className="div-2">
+            <div className="overlap-5">
+              <img className="logo" alt="Logo" src="/assets/img1/logo.png" />
+              <div className="text-wrapper-23">About Us</div>
+              <div className="text-wrapper-24">Blog</div>
+              <div className="text-wrapper-25">Filters</div>
+              <img className="vector-22" alt="Vector" src="/assets/img1/vector.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard
---
+++ src/screens/Home/Home.jsx
@@ -0,0 +1,208 @@
+import React from "react";
+import "./style.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="div">
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="text-wrapper">Let's Clear your Doubts!</div>
+          <div className="group">
+            <div className="overlap-group">
+              <p className="what-is-the-approval">
+                What is the approval criteria to be a Mentor?&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; +
+              </p>
+            </div>
+            <div className="how-does-mentor-wrapper">
+              <p className="how-does-mentor">
+                How does Mentor Bridge work for Mentors?&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+
+              </p>
+            </div>
+            <div className="can-i-charge-fees-of-wrapper">
+              <p className="can-i-charge-fees-of">
+                Can I charge fees of my
+                choice?&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                +
+              </p>
+            </div>
+            <div className="can-i-get-guidance-wrapper">
+              <p className="can-i-get-guidance">
+                Can I get guidance on how to be a mentor ?&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; +
+              </p>
+            </div>
+          </div>
+          <footer className="footer">
+            <div className="overlap-2">
+              <div className="text-wrapper-2">Help center</div>
+              <div className="text-wrapper-3">About us</div>
+              <div className="text-wrapper-4">FAQs</div>
+              <img className="vector" alt="Vector" src="/assets/img/vector-2.svg" />
+              <img className="img" alt="Vector" src="/assets/img/vector-3.svg" />
+              <img className="vector-2" alt="Vector" src="/assets/img/vector-4.svg" />
+              <img className="vector-3" alt="Vector" src="/assets/img/vector-5.svg" />
+              <p className="element-mentorbridge">
+                <span className="span">2023 M</span>
+                <span className="text-wrapper-5">entorbridge. </span>
+                <span className="span">All Rights Reserved.</span>
+              </p>
+              <div className="text-wrapper-6">Contact us</div>
+              <div className="text-wrapper-7">Privacy policy</div>
+              <div className="text-wrapper-8">Terms of use</div>
+              <img className="vector-4" alt="Vector" src="/assets/img/vector-6.svg" />
+            </div>
+          </footer>
+        </div>
+        <div className="overlap-3">
+          <div className="rectangle-2" />
+          <img className="group-2" alt="Group" src="/assets/img/group-2.png" />
+          <img className="group-3" alt="Group" src="/assets/img/group-3.png" />
+          <h1 className="h-1">Grow Faster Together</h1>
+          <p className="find-a-mentor-or">
+            Find a mentor or Lead the path for someone
+            <br />
+            It’s a win win!
+          </p>
+          <img className="blue-rocket" alt="Blue rocket" src="/assets/img/blue-rocket.png" />
+          <img
+            className="girl-on-psychologist"
+            alt="Girl on psychologist"
+            src="/assets/img/girl-on-psychologist-online-session.png"
+          />
+          <div className="top-menu">
+            <div className="overlap-4">
+              <img className="logo" alt="Logo" src="/assets/img/logo.png" />
+              <div className="button">
+                <div className="div-wrapper">
+                  <div className="text-wrapper-9">Login</div>
+                </div>
+              </div>
+              <div className="overlap-wrapper">
+                <div className="overlap-5">
+                  <div className="text-wrapper-10">Signup</div>
+                </div>
+              </div>
+              <div className="text-wrapper-11">About Us</div>
+              <div className="text-wrapper-12">Blog</div>
+              <div className="overlap-group-wrapper">
+                <div className="overlap-6">
+                  <div className="text-wrapper-13">Earning calculator</div>
+                </div>
+              </div>
+              <div className="text-wrapper-14">Filters</div>
+              <img className="vector-5" alt="Vector" src="/assets/img/vector-7.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-7">
+          <div className="text-wrapper-15">Find the expertises from</div>
+          <p className="p">Make your career growth in</p>
+          <div className="frame">
+            <div className="group-4">
+              <div className="group-5">
+                <div className="overlap-group-2">
+                  <div className="text-wrapper-16">UX/UI Design</div>
+                </div>
+              </div>
+              <div className="group-6">
+                <div className="overlap-8">
+                  <div className="text-wrapper-17">Teaching</div>
+                </div>
+              </div>
+              <div className="group-7">
+                <div className="overlap-9">
+                  <div className="text-wrapper-17">Content writing</div>
+                </div>
+              </div>
+              <div className="group-8">
+                <div className="overlap-10">
+                  <div className="text-wrapper-17">Front end</div>
+                </div>
+              </div>
+              <div className="group-9">
+                <div className="overlap-11">
+                  <div className="text-wrapper-18">Data Science</div>
+                </div>
+              </div>
+              <div className="group-10">
+                <div className="product-management-wrapper">
+                  <div className="product-management">
+                    Product <br />
+                    Management
+                  </div>
+                </div>
+              </div>
+              <div className="group-11">
+                <div className="overlap-12">
+                  <div className="text-wrapper-19">Marketing</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group-wrapper">
+            <div className="group-12">
+              <img className="image" alt="Myntra Logo" src="/assets/img/image-2.png" />
+              <img className="rectangle-3" alt="Rectangle" src="/assets/img/rectangle-10.png" />
+              <img className="rectangle-4" alt="Rectangle" src="/assets/img/rectangle-11.png" />
+              <img className="rectangle-5" alt="Rectangle" src="/assets/img/rectangle-12.png" />
+              <img className="image-2" alt="Amazon Logo" src="/assets/img/image-1.png" />
+            </div>
+          </div>
+        </div>
+        <p className="text-wrapper-20">What our Clients says about us</p>
+        <div className="group-13">
+          <div className="overlap-13">
+            <div className="overlap-14">
+              <img className="union" alt="Union" src="/assets/img/union.svg" />
+              <img className="ellipse" alt="Ellipse" src="/assets/img/ellipse-3.png" />
+              <p className="lorem-ipsum-dolor">
+                Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et
+                Dolore Magna Aliqua. Ut Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut Aliquip
+                Ex Ea Commodo Consequat. Duis Aute Irure Dolor In Reprehenderit In Voluptate Velit Esse Cillum Dolore Eu
+                Fugiat Nulla Pariatu
+              </p>
+              <div className="text-wrapper-21">Alisa</div>
+            </div>
+            <div className="founder-XOXO">Founder,xoxo</div>
+          </div>
+        </div>
+        <div className="group-14">
+          <div className="overlap-15">
+            <div className="overlap-16">
+              <img className="union" alt="Union" src="/assets/img/union-1.svg" />
+              <img className="ellipse-2" alt="Ellipse" src="/assets/img/ellipse-3-1.png" />
+              <p className="lorem-ipsum-dolor-2">
+                Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et
+                Dolore Magna Aliqua. Ut Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut Aliquip
+                Ex Ea Commodo Consequat. Duis Aute Irure Dolor In Reprehenderit In Voluptate Velit Esse Cillum Dolore Eu
+                Fugiat Nulla Pariatu
+              </p>
+              <div className="text-wrapper-22">Michel</div>
+            </div>
+            <div className="manager-QWERTY">Manager,qwerty</div>
+          </div>
+        </div>
+        <div className="group-15">
+          <div className="overlap-15">
+            <div className="overlap-16">
+              <img className="union" alt="Union" src="/assets/img/union-2.svg" />
+              <img className="ellipse-2" alt="Ellipse" src="/assets/img/ellipse-3-2.png" />
+              <p className="lorem-ipsum-dolor-3">
+                Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et
+                Dolore Magna Aliqua. Ut Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut Aliquip
+                Ex Ea Commodo Consequat. Duis Aute Irure Dolor In Reprehenderit In Voluptate Velit Esse Cillum Dolore Eu
+                Fugiat Nulla Pariatu
+              </p>
+              <div className="lee-cooper">Lee Cooper</div>
+            </div>
+            <div className="HR-director-meta">Hr Director, Meta</div>
+          </div>
+        </div>
+        <img className="vector-6" alt="Vector" src="/assets/img/vector.svg" />
+        <img className="vector-7" alt="Vector" src="/assets/img/vector-1.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default Home
